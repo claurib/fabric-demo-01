3e1098568af31ebd2ae99bbbdfc7d6143b0f0a07
Claudinei Ribeiro Pinto <65458934+claurib@users.noreply.github.com>
2025-05-04 19:25:25 -0300

    Committing 1 item from workspace 088ee47e-f26d-41cd-bddc-9828f5a20452

---
+++ ProjectFabric/myNotebook.Notebook/notebook-content.py
@@ -0,0 +1,44 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "38a5e889-a3b2-4fc5-9de1-71f8c2cd7f8e",
+# META       "default_lakehouse_name": "Lab2Lakehouse",
+# META       "default_lakehouse_workspace_id": "088ee47e-f26d-41cd-bddc-9828f5a20452",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "38a5e889-a3b2-4fc5-9de1-71f8c2cd7f8e"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+print('Hello world!!')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
